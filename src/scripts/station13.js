29f09ee5d1ddeb60a497de9427ec84222e2f05ad
mana <mnm050125@gmail.com>
2024-08-10 19:47:07 +0900

    Fix typo in station13.js

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if (check.checked){
+    textElement.style.backgroundColor = "red";
+  }
+  else{
+    textElemert.style.backgroundColor = "";
+  }
 }
+
